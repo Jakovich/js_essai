f9a343f8e4c1b0f3e7ecc6be2af9a346c7be9bdd
Nadia Pillard <ua9ond@yandex.ru>
2016-03-28 17:27:29 +0700

    Function SumOfArray replaced by reduce method, var length is not global

--- build/js/check.js
+++ build/js/check.js
@@ -1,26 +1,18 @@
 "use strict";
-var sum = 0;
-var length = 0;
 
-function sumOfArray(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum;
-};
-
-function multiplOfArrays(arr1,arr2) {
-  if (arr1.length>arr2.length){
+function multiplOfArrays(arr1, arr2) {
+  var length = 0;
+  if (arr1.length > arr2.length){
     for (var i = 0; i < arr1.length; i++) {
-    length += arr1[i]*arr2[i];
-  }
-  return length;
+      length += arr1[i] * arr2[i];
+    }
+    return length;
   }
   else {
     for (var i = 0; i < arr2.length; i++) {
-    length += arr2[i]*arr1[i];
-  }
-  return length;
+      length += arr2[i] * arr1[i];
+    }
+    return length;
   }
 };
 
@@ -29,7 +21,7 @@ function getMessage(a, b) {
   switch (typeof a) {
     case "boolean":
       if(a) {
-        var fireSuccesMessage = "Я попал в "+b;
+        var fireSuccesMessage = "Я попал в " + b;
         return fireSuccesMessage;
       }
       else {
@@ -39,19 +31,21 @@ function getMessage(a, b) {
 
     case "number":
       var jumpHeight = a * 100;
-      var jumpMessage = "Я прыгнул на "+jumpHeight+" сантиметров";
+      var jumpMessage = "Я прыгнул на " + jumpHeight + " сантиметров";
       return jumpMessage;
       break;
 
     case "object":
-      if(typeof b == "object"){
+      if (typeof b == "object"){
         var distanceCount= multiplOfArrays(a,b);
-        var distanceMessage = "Я прошёл "+length+" метров"
+        var distanceMessage = "Я прошёл " + distanceCount + " метров"
         return distanceMessage;
       }
       else {
-        var stepCount = sumOfArray(a);
-        var stepMessage =  "Я прошёл "+stepCount+" шагов";
+        var stepCount = a.reduce(function(sum, current) {
+        return sum + current;
+        });
+        var stepMessage =  "Я прошёл "+ stepCount + " шагов";
         return stepMessage;
       };
       break;
