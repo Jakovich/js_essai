c4f669656aac666e912f4ffe0bcb137d9979f67f
Nadia Pillard <ua9ond@yandex.ru>
2016-04-08 19:31:37 +0700

    setTimeofExpires changed

--- src/form.js
+++ src/form.js
@@ -141,12 +141,10 @@
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
     var birthDate = new Date(currentYear, 2, 6);
-    if (currentDate > birthDate) {
-      var expireDateMilisec = currentDate - birthDate;
-    } else {
-      var birthDate = new Date(currentYear - 1, 2, 6);
-      var expireDateMilisec = currentDate - birthDate;
+    if (currentDate <= birthDate) {
+      birthDate = new Date(currentYear - 1, 2, 6);
     }
+    var expireDateMilisec = currentDate - birthDate;;
     var expireDate = Math.floor(expireDateMilisec / 3600 / 24 / 1000);
     return expireDate;
   }
