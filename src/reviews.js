03473b4649964da34e98ab5c44be9fc170b77f15
Nadia Pillard <ua9ond@yandex.ru>
2016-04-18 00:33:26 +0700

    code style checked

--- src/reviews.js
+++ src/reviews.js
@@ -58,23 +58,22 @@
     var xhr = new XMLHttpRequest();
     xhr.timeout = 10000;
   
-    
     xhr.onloadstart = function() {
-      reviewsContainer.classList.add('.reviews-list-loading');
+      reviewsContainer.classList.add('reviews-list-loading');
     };
     
-      xhr.onerror = function() {
-      if (reviewsContainer.classList.contains('.reviews-list-loading')) {
-        reviewsContainer.classList.remove('.reviews-list-loading');
+    xhr.onerror = function() {
+      if (reviewsContainer.classList.contains('reviews-list-loading')) {
+        reviewsContainer.classList.remove('reviews-list-loading');
       }
-      reviewsContainer.classList.add('.reviews-load-failure');
+      reviewsContainer.classList.add('reviews-load-failure');
     };
     
     xhr.ontimeout = xhr.onerror;
     
     xhr.onload = function(event) {
-      if (reviewsContainer.classList.contains('.reviews-list-loading')) {
-        reviewsContainer.classList.remove('.reviews-list-loading');
+      if (reviewsContainer.classList.contains('reviews-list-loading')) {
+        reviewsContainer.classList.remove('reviews-list-loading');
       }
       if (reviewsFilter.classList.contains('invisible')){
         reviewsFilter.classList.remove('invisible');
@@ -130,6 +129,7 @@
           return b.review_usefulness - a.review_usefulness;
         });
         break;
+        
       default:
         reviewsToFilter = reviews.slice(0);
     }
